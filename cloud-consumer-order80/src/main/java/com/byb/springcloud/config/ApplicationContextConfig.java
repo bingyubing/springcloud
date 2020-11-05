e7fcf870da8debbcfb100c5ac9c5b139636bd657
76068 <760683982@qq.com>
2020-11-05 11:23:57 +0800

    子模块order80启动失败

--- cloud-consumer-order80/src/main/java/com/byb/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/byb/springcloud/config/ApplicationContextConfig.java
@@ -1,10 +1,15 @@
 package com.byb.springcloud.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+//@Configuration用于定义配置类，可替换xml配置文件，
+// 被注解的类内部包含有一个或多个被@Bean注解的方法，
+// 这些方法将会被AnnotationConfigApplicationContext或AnnotationConfigWebApplicationContext类进行扫描，并用于构建bean定义，初始化Spring容
+@Slf4j
 public class ApplicationContextConfig {
     @Bean
     public RestTemplate getRestTemplate(){
--- cloud-consumer-order80/src/main/java/com/byb/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/byb/springcloud/controller/OrderController.java
@@ -1,8 +1,27 @@
 package com.byb.springcloud.controller;
 
+import com.byb.springcloud.entities.CommonResult;
+import com.byb.springcloud.entities.Payment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 @RestController
-public class OrderController {
+//@RestController  可以返回多种信息 xml json 等   等于@Controller与 @RequestBody
 
+public class OrderController {
+    public static final String PAYMENT_URL = "http://localhost:8001";
+    @Resource
+    private RestTemplate restTemplate;
+    @GetMapping("/consumer/payment/create")
+    public CommonResult<Payment> create(Payment payment ){
+        return restTemplate.postForObject(PAYMENT_URL+"/payment/create",payment,CommonResult.class);
+    }
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
+        return restTemplate.getForObject(PAYMENT_URL+"payment/get/"+id,CommonResult.class);
+    }
 }
